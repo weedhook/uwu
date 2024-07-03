1b5cfa643c49bb9571e77df6b15cf3713d7c8de2
firm0001 <106458550+firm0001@users.noreply.github.com>
2024-07-02 21:15:18 -0400

    Update auth.lua

--- dependencies/auth.lua
+++ dependencies/auth.lua
@@ -1,26 +1,24 @@
-local Auth             = "https://permit-dn3p.onrender.com"
+local Auth = "https://permit-dn3p.onrender.com"
 
 local function parseJson(response)
-    local success, result = pcall(function() return game:GetService("HttpService"):JSONDecode(response) end)
+    local success, result = pcall(function()
+        return game:GetService("HttpService"):JSONDecode(response)
+    end)
     if success then
         return result
     else
-        return "Executor doesnt Support Parsing."
+        return { error = "Executor doesn't support parsing." }
     end
 end
 
 local function authenticateKey(key)
     local url = Auth .. "/api/authenticate?hash=" .. key
     local response = game:HttpGet(url)
-    return parseJson(response)
+    local result = parseJson(response)
+    if result and result.token ~= nil then
+        print("Authentication successful. Token:", result.token)
+    else
+        game.Players.LocalPlayer:Kick("discord.gg/grimcc")
+    end
 end
-
-local key = nil
-
-local result = authenticateKey(key)
-
-if result and result['token'] ~= nil then
-    print('success;)
-else
-    game.Players.LocalPlayer:Kick("discord.gg/grimcc")
-end
+authenticateKey(...)
